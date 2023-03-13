d6d576c663be12915adbc90667450840d1dd5b7c
Ivan_M <iamartynov@rambler.ru>
2023-03-13 23:54:01 +0300

    simple task for p2p code review

---
+++ test/ReversePolishNotationCalculatorTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class ReversePolishNotationCalculatorTest {
+    private static ReversePolishNotationCalculator reversePolishNotationCalculator;
+
+    @BeforeAll
+    static void beforeAll() {
+        reversePolishNotationCalculator = new ReversePolishNotationCalculator();
+    }
+
+    @Test
+    public void shouldCalculateAddition() {
+        assertEquals(7, reversePolishNotationCalculator.calculatePolishNotation("4 3 +"));
+    }
+
+    @Test
+    public void shouldCalculateSubtraction() {
+        assertEquals(1, reversePolishNotationCalculator.calculatePolishNotation("4 3 -"));
+    }
+
+    @Test
+    public void shouldCalculateProduction() {
+        assertEquals(12, reversePolishNotationCalculator.calculatePolishNotation("4 3 *"));
+    }
+
+    @Test
+    public void shouldCalculateAdditionForOneNegativeOperand() {
+        assertEquals(-1, reversePolishNotationCalculator.calculatePolishNotation("-4 3 +"));
+    }
+
+    @Test
+    public void shouldCalculateSubtractionForOneNegativeOperand() {
+        assertEquals(7, reversePolishNotationCalculator.calculatePolishNotation("4 -3 -"));
+    }
+
+    @Test
+    public void shouldCalculateProductionForOneNegativeOperand() {
+        assertEquals(-12, reversePolishNotationCalculator.calculatePolishNotation("4 -3 *"));
+    }
+
+    @Test
+    public void shouldCalculateCompoundArithmetic() {
+        assertEquals(10, reversePolishNotationCalculator.calculatePolishNotation("-2 -1  4  0 +  -  *"));
+    }
+}
